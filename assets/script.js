6178f07d57f3481e228271cf24ea12242c6f089a
frunox <60527588+frunox@users.noreply.github.com>
2020-03-08 22:04:01 -0500

    removed lis from done page

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,12 @@ var p = document.querySelector('p');
 var button = document.querySelector('button');
 var hr = document.querySelector('hr');
 var h2 = document.querySelector('h2');
+var startMessage = document.querySelector('.startMessage');
 var comeback = document.querySelector('#comeBack');
-var label = document.querySelector('label');
-var input = document.querySelector('input');
+// var label = document.querySelector('label');
+// var input = document.getElementsByClassName('done');
+// var boxName = document.getElementById('boxName');
+var item = document.getElementsByClassName('listItem');
 var questionsIndex = 0;
 var answersIndex = 0;
 var userPoints = 0;
@@ -17,7 +20,13 @@ var id;
 
 // set up arrays with the questions and potential answers to each question
 var questions = ['Commonly used data types DO NOT include:', 'The condition in an if/else statement is enclosed within ________.', 'Arrays in JavaScript can be used to store ________.', 'String values must be enclosed within _______ when being assigned to variables.', 'A very useful too used during development and debugging for printing content to the debugger is:']
-var answers = ['strings', 'booleans', 'alerts', 'numbers', 'commas', 'curly brackets', 'parentheses', 'square brackets', 'numbers and strings', 'other arrays', 'booleans', 'all of the above', 'commas', 'curly brackets', 'quotes', 'parentheses','Javascript', 'terminal/bash', 'for loops', 'console.log'];
+var answers = [
+    ['strings', 'booleans', 'alerts', 'numbers'],
+    ['commas', 'curly brackets', 'parentheses', 'square brackets'],
+    ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    ['Javascript', 'terminal/bash', 'for loops', 'console.log']
+    ];
 var correctAnswersId = [2, 2, 3, 2, 3]
 
 // FUNCTIONS
@@ -28,6 +37,7 @@ function checkAnswer(id) {
     // display the hr
     hr.style.display = 'block';
     // check for correct answer
+    // console.log("questionsIndex before check  " + questionsIndex);
     if(id == correctAnswersId[questionsIndex]) {
         comeback.innerHTML = "Correct!";
         userPoints += 5;
@@ -35,33 +45,17 @@ function checkAnswer(id) {
         comeback.innerHTML = "Wrong";
     }
     questionsIndex++;
-    console.log("questionsIndex  " + questionsIndex);
-    if(questionsIndex === 5) {
-        console.log('IN final loop');
-        // clear answers list
-        while(ol.firstChild) {
-            console.log(ol.firstChild);
-            ol.removeChild(ol.firstChild);
-            console.log("in ol loop");
-        }
-
-        console.log(ol.firstChild);
-        allDone();
-    }
-    answersIndex += 4;
-    
-    // console.log('answersIndex: ' + answersIndex);
-    
+    // console.log("questionsIndex after check  " + questionsIndex);
     displayQuestion();
 }
 
 function displayQuestion() {
-    
-    
     // clear any previous answers
     while(ol.firstChild) {
         ol.removeChild(ol.firstChild);
     }
+    // console.log("questionsIndex before  " + questionsIndex);
+    
     // hide the h1
     h1.style.display = 'none';
     // hide button
@@ -70,33 +64,42 @@ function displayQuestion() {
     p.innerHTML = questions[questionsIndex];
     
     for(var i = 0; i < 4; i++) {
+        // console.log("i: " + i + "  questionsIndex: " + questionsIndex);
+        if(questionsIndex === answers.length){
+            console.log("go to allDone()");
+            ol.style.display = 'none';
+            p.style.display = 'none';
+            hr.style.display = 'none';
+            h2.innerHTML = "All done!";
+            comeback.innerHTML = "Your final score is:  " + userPoints;
+            break;
+        }
         // create list items for answers
         var li = document.createElement("li");
         li.setAttribute("id", i);
+        li.setAttribute("class", "listItem");
         li.setAttribute("onClick", 'checkAnswer(this.id)');
-        li.innerHTML = answers[i + answersIndex];
+        li.innerHTML = answers[questionsIndex][i];
         ol.append(li);
-        
     }
+    
+    // console.log(item);
 }    
 
 
-
 function allDone() {
-    // clear answers list
-    // while(ol.firstChild) {
-    //     ol.removeChild(ol.firstChild);
-    //     console.log("in ol loop");
-    // }
-    // // li.style.display = 'none';  // this doesn't work
     // hide the <p>
-    p.style.display = 'none';
-    hr.style.display = 'none';
-    // comeback.style.display = 'none';
-    // // add text to the h2
-    h2.innerHTML = "All done!";
-    console.log('in allDone()');
-    comeback.innerHTML = "Your final score is:  " + userPoints;
-
     
+    
+    // add text to the h2
+    
+    console.log('in allDone()');
+    
+    // display input box for initials
+    // allDone.style.display = 'block';
+    
+}
+
+function highScores() {
+    console.log("in highScores()");
 }
