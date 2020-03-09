d446b29a427b25cd469bc9a2c819a9b753c801d6
frunox <60527588+frunox@users.noreply.github.com>
2020-03-09 18:46:54 -0500

    question/answer functioning, updated css

--- assets/script.js
+++ assets/script.js
@@ -17,8 +17,6 @@ var questionsIndex = 0;
 var answersIndex = 0;
 var userPoints = 0;
 var id;
-
-
 // set up arrays with the questions and potential answers to each question
 var questions = ['Commonly used data types DO NOT include:', 'The condition in an if/else statement is enclosed within ________.', 'Arrays in JavaScript can be used to store ________.', 'String values must be enclosed within _______ when being assigned to variables.', 'A very useful too used during development and debugging for printing content to the debugger is:']
 var answers = [
@@ -33,12 +31,9 @@ var correctAnswersId = [2, 2, 3, 2, 3]
 // FUNCTIONS
 // check if answer is correct or not
 function checkAnswer(id) {
-    
-    // console.log("in checkAnswer  id = " + id);
     // display the hr
     hr.style.display = 'block';
     // check for correct answer
-    // console.log("questionsIndex before check  " + questionsIndex);
     if(id == correctAnswersId[questionsIndex]) {
         comeback.innerHTML = "Correct!";
         userPoints += 5;
@@ -46,72 +41,80 @@ function checkAnswer(id) {
         comeback.innerHTML = "Wrong";
     }
     questionsIndex++;
-    // console.log("questionsIndex after check  " + questionsIndex);
+    // to function to display the question and possible answers
     displayQuestion();
 }
 
+// display the questions and possible answers
 function displayQuestion() {
     // clear any previous answers
-    while(ol.firstChild) {
-        ol.removeChild(ol.firstChild);
-    }
-    // console.log("questionsIndex before  " + questionsIndex);
-    
+    removeLi();
     // hide the h1
     h1.style.display = 'none';
-    // hide button
+    // hide start button
     startBtn.style.display ="none";
-    // replace text with a question;
+    // put the question into the p tag
     p.innerHTML = questions[questionsIndex];
-    
+    // loop to create list of answers for each question, go to allDone() when all questions are answered
     for(var i = 0; i < 4; i++) {
         // console.log("i: " + i + "  questionsIndex: " + questionsIndex);
         if(questionsIndex === answers.length){
             console.log("go to allDone()");
-            ol.style.display = 'none';
-            p.style.display = 'none';
-            hr.style.display = 'none';
-            h2.innerHTML = "All done!";
-            input.style.display = 'block';
-            comeback.innerHTML = "Your final score is:  " + userPoints;
-            break;
+            removeLi();
+            allDone();
+        } else {
+            // create list items for answers
+            var li = document.createElement("li");
+            li.setAttribute("id", i);
+            li.setAttribute("class", "listItem");
+            li.setAttribute("onClick", 'checkAnswer(this.id)');
+            li.innerHTML = answers[questionsIndex][i];
+            ol.append(li);
         }
-        // create list items for answers
-        var li = document.createElement("li");
-        li.setAttribute("id", i);
-        li.setAttribute("class", "listItem");
-        li.setAttribute("onClick", 'checkAnswer(this.id)');
-        li.innerHTML = answers[questionsIndex][i];
-        ol.append(li);
+    // TODO: pause (setTimeout)    
     }
-    
-    // console.log(item);
 }    
 
-
+// show final score and enter initials to save score
 function allDone() {
-    // hide the <p>
-    
-    
-    // add text to the h2
-    
-    console.log('in allDone()');
-    
+    // remove unnecessary tags
+    p.style.display = 'none';
+    hr.style.display = 'none';
+    // insert message for this page
+    h2.innerHTML = "All done!";
+    // show score
+    comeback.innerHTML = "Your final score is:  " + userPoints;
     // display input box for initials
-    // allDone.style.display = 'block';
-    
+    input.style.display = 'block';
+    //  TODO: capture initials, save with score to local memory
 }
 
+// show high scores
 function highScores() {
-    console.log("in highScores()");
-    h1.style.display = 'block';
-    h1.innerHTML = "High Scores";
+    // remove li tags
+    removeLi();
+    // remove unnecessary tags
+    h2.style.display = 'none';
+    hr.style.display = 'none';
+    comeback.style.display = 'none';
     p.style.display = 'none';
     startBtn.style.display = 'none'
+    // show title for this page
+    h1.style.display = 'block';
+    h1.innerHTML = "High Scores";
+    // TODO:  find highest score and display initials and score
+    //  show buttons
+    // TODO: place high scored, don't show initial input
     scoreButtons.style.display = 'inline-block';
-
+    // TODO:  set Go Back button properly
+    // TODO:  clear the scores
 }
 
-function showScore() {
-    console.log("in showScores()");
+// remove the li tags that show the answers for the previous(or last) question
+function removeLi() {
+    while(ol.firstChild) {
+        ol.removeChild(ol.firstChild);
+    }
+    console.log("in removeLi()");
+    return;
 }
